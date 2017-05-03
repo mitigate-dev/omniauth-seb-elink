d9e83b82b05d0f5f0ba5995b04d9b49478babc67
Māris Ābols <maris.abols@makit.lv>
2017-05-03 16:30:19 +0300

    Fix spec helper, added certs, fix request phase tests

--- spec/omniauth/strategies/seb_spec.rb
+++ spec/omniauth/strategies/seb_spec.rb
@@ -1,41 +1,31 @@
 require 'spec_helper'
 
-describe OmniAuth::Strategies::Nordea do
-  RCVID = '87654321LV'
-  MAC   = 'LEHTI'
+describe OmniAuth::Strategies::Seb do
+  PUBLIC_CRT = File.read(File.join(RSpec.configuration.cert_folder, 'response.public.pem'))
+  SND_ID = 'AAA'
 
   let(:app){ Rack::Builder.new do |b|
-    b.use Rack::Session::Cookie, {:secret => "abc123"}
-    b.use OmniAuth::Strategies::Nordea, RCVID, MAC, hash_algorithm: :md5
+    b.use Rack::Session::Cookie, {secret: 'abc123'}
+    b.use OmniAuth::Strategies::Seb, PUBLIC_CRT, SND_ID
     b.run lambda{|env| [404, {}, ['Not Found']]}
   end.to_app }
 
-  context "request phase" do
+  context 'request phase' do
+    before(:each) { get '/auth/seb' }
 
-    before(:each) { get "/auth/nordea" }
-
-    it "displays a single form" do
+    it 'displays a single form' do
       expect(last_response.status).to eq(200)
       expect(last_response.body.scan('<form').size).to eq(1)
     end
 
-    it "has JavaScript code to submit the form after it's created" do
-      expect(last_response.body).to be_include("</form><script type=\"text/javascript\">document.forms[0].submit();</script>")
+    it 'has JavaScript code to submit the form after it is created' do
+      expect(last_response.body).to be_include('</form><script type="text/javascript">document.forms[0].submit();</script>')
     end
 
     EXPECTED_VALUES = {
-      "A01Y_ACTION_ID" => "701",
-      "A01Y_VERS" => "0002",
-      "A01Y_RCVID" => RCVID,
-      "A01Y_LANGCODE" => "LV",
-      "A01Y_STAMP" => "yyyymmddhhmmssxxxxxx",
-      "A01Y_IDTYPE" => "02",
-      "A01Y_RETLINK" => "http://example.org/auth/nordea/callback?omniauth_status=success",
-      "A01Y_CANLINK" => "http://example.org/auth/nordea/callback?omniauth_status=cancelled",
-      "A01Y_REJLINK" => "http://example.org/auth/nordea/callback?omniauth_status=rejected",
-      "A01Y_KEYVERS" => "0001",
-      "A01Y_ALG" => "01",
-      "A01Y_MAC" => "5EF7613AA29E659456C47D0F9C471470"
+      'IB_SND_ID': SND_ID,
+      'IB_SERVICE': OmniAuth::Strategies::Seb::AUTH_SERVICE,
+      'IB_LANG': 'LAT'
     }
 
     EXPECTED_VALUES.each_pair do |k,v|
@@ -45,15 +35,14 @@ describe OmniAuth::Strategies::Nordea do
         )
       end
     end
-
   end
 
-  context "callback phase" do
+  context 'callback phase' do
     let(:auth_hash){ last_request.env['omniauth.auth'] }
 
     context "with valid response" do
       before do
-        post '/auth/nordea/callback',
+        post '/auth/seb/callback',
           "B02K_ALG": "01",
           "B02K_CUSTID": "37404280367",
           "B02K_CUSTNAME": "RAITUMS ARNIS",
@@ -66,18 +55,32 @@ describe OmniAuth::Strategies::Nordea do
           "B02K_VERS": "0002"
       end
 
-      it "sets the correct uid value in the auth hash" do
+      it 'sets the correct uid value in the auth hash' do
         expect(auth_hash.uid).to eq("374042-80367")
       end
 
-      it "sets the correct info.full_name value in the auth hash" do
+      it 'sets the correct info.full_name value in the auth hash' do
         expect(auth_hash.info.full_name).to eq("ARNIS RAITUMS")
+      end
+    end
+
+    context 'with non-existant public key file' do
+      let(:app){ Rack::Builder.new do |b|
+        b.use Rack::Session::Cookie, {secret: 'abc123'}
+        b.use(OmniAuth::Strategies::Seb, 'missing-public-key-file.pem', SND_ID )
+        b.run lambda{|env| [404, {}, ['Not Found']]}
+      end.to_app }
+
+      it 'redirects to /auth/failure with appropriate query params' do
+        post '/auth/seb/callback' # Params are not important, because we're testing public key loading
+        expect(last_response.status).to eq(302)
+        expect(last_response.headers['Location']).to eq('/auth/failure?message=public_crt_load_err&strategy=seb')
       end
     end
 
     context "with invalid MAC" do
       before do
-        post '/auth/nordea/callback',
+        post '/auth/seb/callback',
           "B02K_ALG": "01",
           "B02K_CUSTID": "37404280367",
           "B02K_CUSTNAME": "RAITUMS ARNIS",
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,9 +3,12 @@ $:.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec'
 require 'rack/test'
 require 'omniauth'
-require 'omniauth-nordea'
+require 'omniauth-seb-elink'
 
 RSpec.configure do |config|
+  config.add_setting('cert_folder')
+  config.cert_folder = File.expand_path('../certs', __FILE__)
+
   config.include Rack::Test::Methods
   config.extend  OmniAuth::Test::StrategyMacros, :type => :strategy
   config.expect_with :rspec do |c|
